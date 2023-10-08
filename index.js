c4256e288f7c177dca2bed11cd5baa751ee8a446
aprilgom <aprilgom@gmail.com>
2023-10-09 08:44:33 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import { Application, autoDetectRenderer } from 'pixi.js';
+
+const app = new Application();
+
+(async () => {
+    await app.init({
+        background: '#1099bb',
+        resizeTo: window,
+    });
+})
+
+document.body.appendChild(app.view);
