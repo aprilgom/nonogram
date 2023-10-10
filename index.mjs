d985099f79e030d323edf5013f7c2087b87ef78c
aprilgom <aprilgom@gmail.com>
2023-10-11 08:51:39 +0900

    import fix

---
+++ index.mjs
@@ -0,0 +1,23 @@
+import { Application } from "./pixi.mjs";
+
+/*
+const app = new Application();
+
+(async () => {
+    await app.init({
+        background: '#1099bb',
+        resizeTo: window,
+    });
+})
+
+document.body.appendChild(app.view);
+
+const bunny = Sprite.from('https://pixijs.com/assets/bunny.png');
+
+app.stage.addChild(bunny);
+
+bunny.anchor.set(0.5);
+
+bunny.x = app.screen.width / 2;
+bunny.y = app.screen.height / 2;
+*/
